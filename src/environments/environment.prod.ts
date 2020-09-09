88d97e7892bdd91d2d55ef6590dbfccc8e7a1d73
Yemi Tula <yemi@tulabyte.net>
2020-09-09 15:47:47 +0100

    Add API url to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: 'https://tulabyte-geofence-api.herokuapp.com/index.php'
+  // API_URL: 'https://tulabyte-geofence-api.herokuapp.com/index.php',
+  API_URL: 'https://tulabyte.net/geofence-api/index.php', // tulabyte.net
 };
